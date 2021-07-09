623ea2a720100b77088bae9b4b7c428a244ba887
KyungjinJPark <kjparkcontact@gmail.com>
2021-07-09 00:50:17 -0400

    can now click on pixels to toggle color, and changes will be saved to data/grid.json

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,16 +2,46 @@ const setUpGrid = (width, height, pixels) => {
   let gridDiv = document.getElementsByClassName("pixel-grid");
   gridDiv[0].style.width = width * 2 + "em";
   gridDiv[0].style.height = height * 2 + "em";
-  console.log("b4 forEach");
-  pixels.forEach((pi) => {
-    console.log("in forEach");
+  pixels.forEach((pi, i) => {
     let pixelDiv = document.createElement("div");
     pixelDiv.className = "pixel";
+
+    // Pixel color stuff
+    let pixelColor = null;
     if (pi === 1) {
-      pixelDiv.style.backgroundColor = "#F00";
+      pixelColor = "#F00";
     } else {
-      pixelDiv.style.backgroundColor = "#FFF";
+      pixelColor = "#FFF";
     }
+    pixelDiv.style.backgroundColor = pixelColor;
+
+    pixelDiv.onmouseenter = () => {
+      pixelDiv.style.backgroundColor = "greenyellow";
+    };
+    pixelDiv.onmouseout = () => {
+      pixelDiv.style.backgroundColor = pixelColor;
+    };
+
+    // On change color (server request)
+    pixelDiv.onclick = () => {
+      const Http = new XMLHttpRequest();
+      const url = "/edit/";
+      Http.open("POST", url, true);
+      Http.setRequestHeader("Content-Type", "application/json");
+      Http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          // IDK what this 4 is...
+          let resp = this.responseText;
+          console.log("DEBUG:", resp);
+          location.reload();
+        }
+      };
+      let data = {
+        PixelId: i,
+      };
+      Http.send(JSON.stringify(data));
+    };
+
     gridDiv[0].appendChild(pixelDiv);
   });
 };
--- webserver.go
+++ webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"strings"
@@ -25,6 +26,7 @@ func loadGridData() (*gridData, error) {
 	if err != nil {
 		return nil, err
 	}
+	// TODO: change this to Unmarshal
 	dec := json.NewDecoder(strings.NewReader(string(gridBytes)))
 	var gdata gridData
 	if err := dec.Decode(&gdata); err == io.EOF {
@@ -40,13 +42,74 @@ var indexTmpl = template.Must(template.ParseFiles("index.html"))
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	gdata, err := loadGridData()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error leading grid data: %v", err)
+		http.Error(w, "can't load grid data", http.StatusInternalServerError)
 		return
 	}
 	indexTmpl.Execute(w, gdata)
 }
 
+// Loads grid dimensions and pixel information
+// Code stolen from golang json docs
+func savePixelData(index int) error {
+	// Get data
+	filename := "data/grid.json"
+	jsonBlob, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	var gdata gridData
+	err = json.Unmarshal(jsonBlob, &gdata)
+	if err != nil {
+		return err
+	}
+
+	if gdata.Pixels[index] == 1 {
+		gdata.Pixels[index] = 0
+	} else {
+		gdata.Pixels[index] = 1
+	}
+	
+	// Write out
+	data, err := json.Marshal(gdata)
+	if err != nil {
+		return err
+	}
+	ioutil.WriteFile(filename, data, 0600)
+	return nil
+}
+
+func editPixelHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+	
+	// May want to make this a seperate function 
+	type message struct {
+		PixelId int
+	}
+	var m message
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		log.Printf("Error decoding body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+
+	err = savePixelData(m.PixelId)
+	if err != nil {
+		log.Printf("Error saving grid data: %v", err)
+		http.Error(w, "can't save grid data", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Success!")
+}
+
 func main() {
+	http.HandleFunc("/edit/", editPixelHandler)
 	http.HandleFunc("/", homeHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
